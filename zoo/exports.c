491072fd0c1bc258f513a92298095f3b2457120d
BrandonFrei <brandon.freiberger98@gmail.com>
2024-09-11 21:26:22 -0700

    Updated cx exports to match linux

--- zoo/exports.c
+++ zoo/exports.c
@@ -3,22 +3,22 @@
 int32_t (*cx_func_error[]) (int32_t, int32_t, int32_t) = {0};
 
 cx_func_stub_t cx_funcs[MAX_NUM_CXUS] = {
-    cx_func_muldiv,
     cx_func_addsub,
+    cx_func_muldiv,
     cx_func_mulacc, 
     cx_func_pext
     };
 
 int32_t num_cfs[MAX_NUM_CXUS] = {
-    CX_MULDIV_NUM_FUNCS, 
     CX_ADDSUB_NUM_FUNCS,
+    CX_MULDIV_NUM_FUNCS,
     CX_MULACC_NUM_FUNCS, 
     CX_PEXT_NUM_FUNCS
     };
 
 int32_t num_states[MAX_NUM_CXUS] = {
-    CX_MULDIV_NUM_STATES,
     CX_ADDSUB_NUM_STATES,
+    CX_MULDIV_NUM_STATES,
     CX_MULACC_NUM_STATES,
     CX_PEXT_NUM_STATES
 };
