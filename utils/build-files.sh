68064e51584ca04eed76094876bbf5b250af6820
BrandonFrei <brandon.freiberger98@gmail.com>
2024-09-11 21:33:58 -0700

    Updated build  for xecuteables

--- utils/build-files.sh
+++ utils/build-files.sh
@@ -6,6 +6,9 @@ pushd initramfs
 pushd home
 riscv32-unknown-linux-gnu-gcc -static `pwd`/../../../examples/open.c -o open -L `pwd`/../../../build/lib/ -lci
 riscv32-unknown-linux-gnu-gcc -static `pwd`/../../../examples/cx_reg.c -o reg -L `pwd`/../../../build/lib/ -lci
+riscv32-unknown-linux-gnu-gcc -static `pwd`/../../../examples/fork.c -o fork -L `pwd`/../../../build/lib/ -lci
+riscv32-unknown-linux-gnu-gcc -static `pwd`/../../../examples/stateless.c -o sl -L `pwd`/../../../build/lib/ -lci
+
 
 popd
 find . -print0 | cpio --null -ov --format=newc | gzip -9 > initramfs.cpio.gz
